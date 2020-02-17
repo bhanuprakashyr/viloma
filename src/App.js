17a3a7aefc67465c49de44c810ec3fc490e2a3e5
prakashbhanu57 <prakashbhanu57@gmail.com>
2020-02-17 23:27:45 +0530

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Paragraph = styled.p`
+  background-color:green
+  `
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Paragraph>
           Edit  <code>src/App.js</code> and save to reload.
-        </p>
+        </Paragraph>
         <a
           className="App-link"
           href="https://reactjs.org"
